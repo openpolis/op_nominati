a132253e87fdaafe3bdaf5f65fa3bc2e23444946
Guglielmo <guglielmo.celata@gmail.com>
2012-05-18 16:36:00 +0200

    databrowse templates and initial redirect adjusted

--- nominati/urls.py
+++ nominati/urls.py
@@ -1,13 +1,16 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib import databrowse
+from django.views.generic.simple import redirect_to
+
 from nominati.models import *
 
 admin.autodiscover()
 databrowse.site.register(Comparto, Regione, Ente, Partecipata, Persona, Incarico)
 
 urlpatterns = patterns('',
-    url(r'^(.*)', databrowse.site.root),
+    url(r'^$', redirect_to, {'url': '/databrowse/'}),
+    url(r'^databrowse/(.*)', databrowse.site.root),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
