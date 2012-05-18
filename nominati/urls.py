8bfcf34b9e9b0e568979ac22d7162803ff8ea3b4
Guglielmo <guglielmo.celata@gmail.com>
2012-05-18 15:21:53 +0200

    databrowse application is accessed in home page

--- nominati/urls.py
+++ nominati/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib import databrowse
+from nominati.models import *
+
 admin.autodiscover()
+databrowse.site.register(Comparto, Regione, Ente, Partecipata, Persona, Incarico)
 
 urlpatterns = patterns('',
+    url(r'^(.*)', databrowse.site.root),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
