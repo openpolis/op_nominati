d2884226ddb0b017563dcdbf7b0a59e31a037ae7
verganis <stefano.vergani.it@gmail.com>
2012-09-28 18:46:11 +0200

    Fixed bug numero partecipate Regione

--- nominati/views.py
+++ nominati/views.py
@@ -93,9 +93,8 @@ class RegioneDetailView(AccessControlView, DetailView):
         now = datetime.now()
         context['table'] = []
         partecipate = Partecipata.objects.filter(ente__regione = r).distinct()
-        if tipologia == '':
-            context['n_partecipate'] = Partecipata.objects.filter(ente__regione = r).distinct().count()
-        else:
+        context['n_partecipate'] = Partecipata.objects.filter(ente__regione = r).distinct().count()
+        if tipologia != '':
             if tipologia =='part_tipologia':
                 context['table'] = partecipate.order_by('tipologia_partecipata')
                 self.template_name = "nominati/part_tipologia.html"
